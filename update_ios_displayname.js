68715a15ac1f75c824d7cfca2f975b1df62f61ad
OmniIsg Build <omniisgbuild@OmniIsgs-Mini.home>
2015-06-25 14:30:54 +0100

    cleaned up and added logging

--- update_ios_displayname.js
+++ update_ios_displayname.js
@@ -3,6 +3,10 @@
 var fs = require('fs');
 var path = require('path');
 
+function log(message){
+	console.log(' > update_ios_display hook: ' + message);
+}
+
 module.exports = function(context){
 	
 	var rootdir = context.opts.projectRoot;
@@ -11,55 +15,63 @@ module.exports = function(context){
 	var projectName = config.name();
 	var xmlHelper = context.requireCordovaModule("cordova-lib/src/util/xml-helpers");	
 
+	/*
+	* this method presumes the plist has a <key> tag with the value CFBundleDisplayName and the
+	* next tag is a <string> with the value of the name to display under the icon
+	*/
 	function findCurrentBundleName(filename){
 		var testXml = xmlHelper.parseElementtreeSync(filename);
 		var pListKeys = testXml.findall('.//key');
 		var pListStrings = testXml.findall('.//string');
-		//console.log(pListStrings);
 		for(var i=0;i<pListKeys.length;i++){
 			if(pListKeys[i].text === "CFBundleDisplayName"){
 
 				var bundleDisplayId = pListKeys[i]._id+1;
 				
 				for(var j=0; j<pListStrings.length;j++){
-					if(pListStrings[j]._id == bundleDisplayId){
-						console.log(pListStrings[j].text);
+					if(pListStrings[j]._id == bundleDisplayId){						
 						return pListStrings[j].text;
 					}
 				}
-			}
+			}			
 		}
 	}
 
+	/*
+	* this method only replaces the first instance found so we are presuming that the first instance of
+	* <string>display name</string> is the one we care about. 
+	* If cordova/apple ever change the order of the plist
+	* this will need rewriting
+	*/
 	function replace_string_in_file(filename, to_replace, replace_with) {
 
 		var data = fs.readFileSync(filename, 'utf8');
 		var indexfound = data.indexOf(to_replace);
 		var result = data.replace(to_replace, replace_with);
 		fs.writeFileSync(filename, result, 'utf8');
-		console.log('changed name of ios display name to: ' + replace_with);
+		log('changed name of ios display name from: ' + to_replace + ' to: ' + replace_with);
 			
 	}
 	
 	//check we have ios platform in project
 	if(context.opts.platforms.indexOf('ios') < 0){
-		console.log('ios platform not found - hook terminating');
+		log('ios platform not found - hook terminating');
 		return;
 	}
 	
 	//get the name from config.xml <preference name="DisplayName" value="Foo"/> node
 	var appName = config.getPreference("DisplayName");
 	if (!appName) {
-		console.log('no ios display name specified in config.xml. Add <preference name="DisplayName" value="Foo"/> to specify a display name');
+		log('no ios display name specified in config.xml. Add <preference name="DisplayName" value="Foo"/> to specify a display name');
 		return;
 	}
 	if (rootdir) {
 			 
-		
+		//leaving as array in case ever add some Android apk renaming	 
 		var filestoreplace = [        
-			// ios
 			"platforms/ios/" + projectName + "/" + projectName + "-Info.plist",
 		];
+
 		filestoreplace.forEach(function(val, index, array) {
 			var fullfilename = path.join(rootdir, val);
 			if (fs.existsSync(fullfilename)) {
@@ -68,11 +80,11 @@ module.exports = function(context){
 				var replacement = '<string>' + appName +'</string>';
 				replace_string_in_file(fullfilename, toReplace,replacement);
 			} else {
-				console.log("could not find: "+fullfilename);
+				log("could not find: "+fullfilename);
 			}
 		});
 	}
 	else{
-		console.log('could not find rootdir');
+		log('could not find rootdir');
 	}
 }
